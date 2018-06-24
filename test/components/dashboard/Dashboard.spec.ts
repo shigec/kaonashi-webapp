444f4869b664ce1ceeca985d3dbc18faf2c4041b
admin <admin@HyperMacBookAirEx.local>
2018-06-24 20:56:54 +0900

    Unit test.

---
+++ test/components/dashboard/Dashboard.spec.ts
@@ -0,0 +1,11 @@
+// import Dashboard from '../../../src/components/dashboard/Dashboard';
+
+// let dashboard = require('../../../src/components/dashboard/Dashboard');
+describe('Dashboard.ts Test', ()=> {
+  it('countUp', ()=> {
+    let i: number = 1;
+    // expect(dashboard.getCount()).toBe(0);
+    // dashboard.countUp();
+    // expect(dashboard.getCount()).toBe(1);
+  })
+})
