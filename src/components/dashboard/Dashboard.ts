90622d7c134e2b25083ae2e66dd2e67c9a876610
nakamu-mu <admin@HyperMacBookAirEx.local>
2018-07-14 20:56:30 +0900

    Implement dashboard component.

--- src/components/dashboard/Dashboard.ts
+++ src/components/dashboard/Dashboard.ts
@@ -37,4 +37,59 @@ export default class Dashboard extends CompomentBase {
     store.commit('setSampleText', 'hage');
   }
 
+  public getWhatsNewList(): any {
+    return [
+      {
+        date: '2018-07-13',
+        kind: 'member',
+        text:  'ほげ　ほげたろうが新規登録されました。',
+      },
+      {
+        date: '2018-07-13',
+        kind: 'member',
+        text:  'ほげ　ほげじろうが新規登録されました。',
+      },
+      {
+        date: '2018-07-12',
+        kind: 'project',
+        text:  'ID基盤PJ (SNC品川シーサイド)',
+      },
+      {
+        date: '2018-07-12',
+        kind: 'system',
+        text:  'メンテナンス情報 2018-07-26 02:00〜05:00',
+      },
+      {
+        date: '2018-07-11',
+        kind: 'project',
+        text:  'ID基盤開発 (SNC品川シーサイド)が追加されました。',
+      },
+      {
+        date: '2018-07-11',
+        kind: 'project',
+        text:  'ビッグデータ解析 (SNC品川シーサイド)が追加されました。',
+      },
+      {
+        date: '2018-07-11',
+        kind: 'member',
+        text:  'ほげ　ほげざぶろうが新規登録されました。',
+      },
+      {
+        date: '2018-07-10',
+        kind: 'member',
+        text:  'ほげ　ほげしろうが削除されました。',
+      },
+      {
+        date: '2018-07-09',
+        kind: 'system',
+        text:  'メンテナンス情報 2018-07-19 02:00〜05:00',
+      },
+      {
+        date: '2018-07-09',
+        kind: 'project',
+        text:  'TVメタ提供サービス開発 (SNC大崎)が追加されました。',
+      }
+    ];
+  }
+
 }
